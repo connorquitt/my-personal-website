e1421ba6a6679535d27ef8c574a619fd6df37f2b
connorq <connorquitt@gmail.com>
2023-07-12 21:11:19 -0500

    centered

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const smashLink = document.getElementById("smashbros");
-smashLink.addEventListener(`onclick`, function() {
- 
+const chinaBob = document.getElementById(`doo1`);
+chinaBob.addEventListener(`onclick`, function() {
+ alert("besties at China Bob");
+})
+
+const yellowhammer = document.getElementById("doo2");
+yellowhammer.addEventListener(`onclick`, function() {
+    alert("Everyone at our regions first big event!");
+})
+
+const georgia = document.getElementById("doo3");
+georgia.addEventListener(`onclick`, function() {
+    console.log("working");
+    alert("Carpool to Georgia!");
 })
